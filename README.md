b29f0083afd0ae9ed2f2be4c20138614fd318952
GemsZen <GemsZen@users.noreply.github.com>
2023-05-08 08:54:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# moedas
+ conerversor de comedas com as principais ccoedas e valores de 08/05/2023
---
+++ conversor-moedas.py
@@ -0,0 +1,50 @@
+real = float(input('Quanto dinheiro você tem na carteira? R$'))
+
+dolar = real / 5.52
+euro = real / 6.19
+libra_esterlina = real / 7.95
+iene_japones = real / 0.05
+franco_suico = real / 6.01
+dolar_australiano = real / 4.16
+dolar_canadense = real / 4.54
+yuan_chines = real / 0.85
+coroa_dinamarquesa = real / 0.88
+dolar_hong_kong = real / 0.71
+forint_hungaro = real / 0.02
+rupia_indiana = real / 0.07
+iaque_cazaquistao = real / 0.01
+peso_mexicano = real / 0.28
+coroa_norueguesa = real / 0.68
+zloty_polones = real / 1.44
+rublo_russo = real / 0.07
+riyal_saudita = real / 1.47
+dolar_cingapura = real / 4.07
+rand_sul_africano = real / 0.37
+coroa_sueca = real / 0.63
+novo_dolar_taiwan = real / 0.18
+lira_turca = real / 0.64
+
+print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
+print('Com R${:.2f} você pode comprar €{:.2f}'.format(real, euro))
+print('Com R${:.2f} você pode comprar £{:.2f}'.format(real, libra_esterlina))
+print('Com R${:.2f} você pode comprar ¥{:.2f}'.format(real, iene_japones))
+print('Com R${:.2f} você pode comprar CHF{:.2f}'.format(real, franco_suico))
+print('Com R${:.2f} você pode comprar AUD${:.2f}'.format(real, dolar_australiano))
+print('Com R${:.2f} você pode comprar CAD${:.2f}'.format(real, dolar_canadense))
+print('Com R${:.2f} você pode comprar CNY{:.2f}'.format(real, yuan_chines))
+print('Com R${:.2f} você pode comprar DKK{:.2f}'.format(real, coroa_dinamarquesa))
+print('Com R${:.2f} você pode comprar HKD${:.2f}'.format(real, dolar_hong_kong))
+print('Com R${:.2f} você pode comprar HUF{:.2f}'.format(real, forint_hungaro))
+print('Com R${:.2f} você pode comprar INR{:.2f}'.format(real, rupia_indiana))
+print('Com R${:.2f} você pode comprar KZT{:.2f}'.format(real, iaque_cazaquistao))
+print('Com R${:.2f} você pode comprar MXN{:.2f}'.format(real, peso_mexicano))
+print('Com R${:.2f} você pode comprar NOK{:.2f}'.format(real, coroa_norueguesa))
+print('Com R${:.2f} você pode comprar HUF{:.2f}'.format(real, zloty_polones))
+print('Com R${:.2f} você pode comprar INR{:.2f}'.format(real, rublo_russo))
+print('Com R${:.2f} você pode comprar KZT{:.2f}'.format(real, riyal_saudita))
+print('Com R${:.2f} você pode comprar MXN{:.2f}'.format(real, peso_mexicano))
+print('Com R${:.2f} você pode comprar NOK{:.2f}'.format(real, dolar_cingapura))
+print('Com R${:.2f} você pode comprar INR{:.2f}'.format(real, rand_sul_africano))
+print('Com R${:.2f} você pode comprar KZT{:.2f}'.format(real, coroa_sueca))
+print('Com R${:.2f} você pode comprar MXN{:.2f}'.format(real, novo_dolar_taiwan))
+print('Com R${:.2f} você pode comprar NOK{:.2f}'.format(real, lira_turca)) 
