dbde8f7d879b006f441d8f543278f580f6efa525
GemsZen <GemsZen@users.noreply.github.com>
2023-06-13 23:47:46 -0300

    Update conversor-moedas.py

--- conversor-moedas.py
+++ conversor-moedas.py
@@ -32,6 +32,7 @@ lira_turca = real / 0.64
 
 
 
+
 print('Com R${:.2f} você pode comprar US${:.2f}'.format(real, dolar))
 print('Com R${:.2f} você pode comprar €{:.2f}'.format(real, euro))
 print('Com R${:.2f} você pode comprar £{:.2f}'.format(real, libra_esterlina))
